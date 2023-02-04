3120985eb9396cee5e2f10ebb5a7c0f3fcb36b26
uyentrang2247 <truongthiuyentrang@gmail.com>
2023-02-04 12:30:26 +0700

    first commit

---
+++ src/main/java/com/unigrad/funiverseworkspacemanagementservice/FuniverseWorkspaceManagementServiceApplication.java
@@ -0,0 +1,15 @@
+package com.unigrad.funiverseworkspacemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class FuniverseWorkspaceManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuniverseWorkspaceManagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/unigrad/funiverseworkspacemanagementservice/WorkspaceManagementController.java
@@ -0,0 +1,18 @@
+package com.unigrad.funiverseworkspacemanagementservice;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+@RequestMapping("/workspace-management")
+public class WorkspaceManagementController {
+    @GetMapping
+    public ResponseEntity<String> getStringHello() throws UnknownHostException {
+        return ResponseEntity.ok("Workspace Management: "+ InetAddress.getLocalHost().getHostName());
+    }
+}

80298fdc3a08acf530ea2aa22331e8173ba162ca
nqbao11 <nqbao11.01@gmail.com>
2023-03-08 02:41:36 +0000

    FUN228

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/FUnigrad/funiverse-workspace-service/internal/goclient"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+	r.GET("/pods", func(ctx *gin.Context) {
+		data, err := goclient.GetPods()
+		if err != nil {
+			ctx.JSON(500, err.Error())
+		}
+		ctx.JSON(200, data)
+	})
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "ok",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	r.Run("0.0.0.0:8080") // listen and serve on 0.0.0.0:8080
 }
